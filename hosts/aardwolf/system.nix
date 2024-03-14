68108426f858bc9a658b9cc20219dff49307cec4
David Koch Gregersen <david.koch@oakdigital.dk>
2024-03-14 15:23:44 +0100

    chore: update aardwolf

--- hosts/aardwolf/system.nix
+++ hosts/aardwolf/system.nix
@@ -14,6 +14,8 @@
     ../../modules/system/locale-dk.nix
     ../../modules/system/fonts.nix
     ../../modules/system/plasma.nix
+    ../../modules/system/bluetooth.nix
+    ../../modules/system/sound.nix
     ../../modules/system/tailscale.nix
   ];
 
@@ -22,4 +24,14 @@
   fonts.enable = true;
   plasma.enable = true;
   tailscale.enable = true;
+  bluetooth.enable = true;
+  sound.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
