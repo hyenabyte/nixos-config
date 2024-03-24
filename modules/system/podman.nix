8e252a485b96c6852905c14199111d28eb2a646b
David Koch Gregersen <david.koch@oakdigital.dk>
2024-03-24 21:41:30 +0100

    feat: adding podman-compose

--- modules/system/podman.nix
+++ modules/system/podman.nix
@@ -18,5 +18,8 @@ in {
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+    environment.systemPackages = with pkgs; [
+      podman-compose
+    ];
   };
 }
