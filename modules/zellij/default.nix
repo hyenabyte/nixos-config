8cc1b72dbe83a1bdf10fb7f18e83513341b998f4
hyenabyte <hyena@hyenabyte.dev>
2024-04-26 12:55:12 +0200

    feat(zellij): no longer open zellij on new shell

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -11,7 +11,7 @@ in {
   config = mkIf cfg.enable {
     programs.zellij = {
       enable = true;
-      enableZshIntegration = true;
+      # enableZshIntegration = true;
 
       settings = {
         theme = "everforest-dark";
