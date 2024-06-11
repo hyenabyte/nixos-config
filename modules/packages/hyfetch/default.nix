3ea29dcda68a71969730b6348f4a686cdce9d699
hyenabyte <hyena@hyenabyte.dev>
2024-06-11 16:29:38 +0200

    feat(package): adding hyfetch

---
+++ modules/packages/hyfetch/default.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.hyfetch;
+in {
+  options.modules.hyfetch = {enable = mkEnableOption "hyfetch";};
+  config = mkIf cfg.enable {
+    programs.hyfetch = {
+      enable = true;
+      settings = {
+        preset = "agender";
+        mode = "rgb";
+        light_dark = "dark";
+        lightness = 0.65;
+        color_align = {
+          mode = "horizontal";
+        };
+      };
+    };
+  };
+}
