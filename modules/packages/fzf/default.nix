d5d51fe6f51dccff54677f7f80db1b1632fbeff1
hyenabyte <hyena@hyenabyte.dev>
2024-06-18 10:55:44 +0200

    feat(packages): add fzf module

---
+++ modules/packages/fzf/default.nix
@@ -0,0 +1,16 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.fzf;
+in {
+  options.modules.fzf = {enable = mkEnableOption "fzf";};
+  config = mkIf cfg.enable {
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+}
