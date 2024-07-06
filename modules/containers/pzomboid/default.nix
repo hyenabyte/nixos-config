b9acd8d1322aca0a6f30bff32be2512d6f95d8be
hyenabyte <hyena@hyenabyte.dev>
2024-07-06 12:08:11 +0200

    update(pzomboid): environment now an age secret

--- modules/containers/pzomboid/default.nix
+++ modules/containers/pzomboid/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -27,31 +26,9 @@ in {
           "/home/hyena/pzomboid/ZomboidDedicatedServer:/home/steam/ZomboidDedicatedServer"
           "/home/hyena/pzomboid/ZomboidConfig:/home/steam/Zomboid/"
         ];
-        environment = {
-          # TODO: store these securely
-          ADMIN_PASSWORD = "changeme";
-          ADMIN_USERNAME = "superuser";
-          AUTOSAVE_INTERVAL = "15";
-          BIND_IP = "0.0.0.0";
-          DEFAULT_PORT = "16261";
-          GAME_VERSION = "public";
-          GC_CONFIG = "ZGC";
-          MAP_NAMES = "Muldraugh, KY";
-          MAX_PLAYERS = "16";
-          MAX_RAM = "4096m";
-          MOD_NAMES = "";
-          MOD_WORKSHOP_IDS = "";
-          PAUSE_ON_EMPTY = "true";
-          PUBLIC_SERVER = "true";
-          RCON_PASSWORD = "changeme_rcon";
-          RCON_PORT = "27015";
-          SERVER_NAME = "ZomboidServer";
-          SERVER_PASSWORD = "";
-          STEAM_VAC = "true";
-          UDP_PORT = "16262";
-          USE_STEAM = "true";
-          TZ = "UTC";
-        };
+        environmentFiles = [
+          config.age.secrets.pzomboid.path
+        ];
       };
     };
   };
