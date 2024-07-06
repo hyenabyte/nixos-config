f7b60308221769c2ecb347a468a367875d205da7
hyenabyte <hyena@hyenabyte.dev>
2024-07-06 13:36:43 +0200

    fix(darwin): adding agenix cli tool

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{inputs, ...}: {
   nixpkgs = {
     config = {
       allowUnfree = true;
@@ -8,4 +8,8 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.max-jobs = "auto";
+
+  environment.systemPackages = [
+    inputs.agenix.packages.aarch64-darwin.default
+  ];
 }
