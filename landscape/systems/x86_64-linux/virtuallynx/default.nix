d82d7f9fc1e493268049b2b2cbe59ff486896e25
hyenabyte <hyena@hyenabyte.dev>
2024-07-08 10:58:20 +0200

    feat(virtuallynx): update config

--- landscape/systems/x86_64-linux/virtuallynx/default.nix
+++ landscape/systems/x86_64-linux/virtuallynx/default.nix
@@ -1,10 +1,23 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; {
   # Virtual Machine Testing Platform
 
   imports = [
     # System hardware configuration
     ./hardware-configuration.nix
   ];
+
+  ${namespace} = {
+    system.nix = enabled;
+    suites.common = enabled;
+    services.ssh = enabled;
+  };
 
   # ! DO NOT CHANGE !
   system.stateVersion = "24.05";
