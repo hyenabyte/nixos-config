ead45b3c0f5936f0fcb6153c0416f41d694593df
hyena <hyena@hyenabyte.dev>
2024-08-08 11:59:59 +0200

    chore(lazygit): adding custom commands

--- landscape/modules/home/cli/lazygit/default.nix
+++ landscape/modules/home/cli/lazygit/default.nix
@@ -12,6 +12,44 @@ in {
   config = mkIf cfg.enable {
     programs.lazygit = {
       enable = true;
+      settings = {
+        customCommands = [
+          {
+            # Push to specific repository
+            # https://github.com/jesseduffield/lazygit/wiki/Custom-Commands-Compendium#pushing-to-a-specific-remote-repository
+            key = "<c-P>";
+            description = "Push to a specific remote repository";
+            context = "global";
+            loadingText = "Pushing ...";
+            command = "git {{index .PromptResponses 1}} {{index .PromptResponses 0}}";
+            prompts = [
+              {
+                type = "menuFromCommand";
+                title = "Which remote repository to push to?";
+                command = "bash -c \"git remote --verbose | grep '/.* (push)'\"";
+                filter = "(?P<remote>.*)\\s+(?P<url>.*) \\(push\\)";
+                valueFormat = "{{ .remote }}";
+                labelFormat = "{{ .remote | bold | cyan }} {{ .url }}";
+              }
+              {
+                type = "menu";
+                title = "How to push?";
+                options = [
+                  {
+                    value = "push";
+                  }
+                  {
+                    value = "push --force-with-lease";
+                  }
+                  {
+                    value = "push --force";
+                  }
+                ];
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
