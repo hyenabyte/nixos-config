3f225c4ac1514866acea35f9122c71408d644794
hyena <hyena@hyenabyte.dev>
2024-08-08 11:56:29 +0200

    feat: adding discord module

---
+++ landscape/modules/home/apps/discord/default.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  lib,
+  config,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.apps.discord;
+in {
+  options.${namespace}.apps.discord = {enable = mkEnableOption "Enable Discord";};
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      discord
+    ];
+  };
+}
