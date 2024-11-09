20c4b42ca0ef4937af6e22a804a8298c3f7157e9
hyena <hyena@hyenabyte.dev>
2024-11-09 20:00:49 +0100

    feat(zed): adding zed module

---
+++ landscape/modules/home/apps/zed/default.nix
@@ -0,0 +1,117 @@
+{
+  pkgs,
+  lib,
+  config,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.apps.zed;
+
+  jsonFormat = pkgs.formats.json {};
+
+  settings = {
+    show_inline_completions = false;
+    assistant = {
+        default_model = {
+        provider = "zed.dev";
+        model = "claude-3-5-sonnet-latest";
+        };
+        dock = "left";
+        version = "2";
+    };
+
+    features = {
+        inline_completion_provider = "supermaven";
+    };
+
+    telemetry = {
+        metrics = false;
+    };
+
+    project_panel = {
+        dock = "right";
+    };
+
+    theme = "Everforest Dark";
+    ui_font_size = 16;
+    buffer_font_size = 16;
+    buffer_font_family = "Agave Nerd Font Mono";
+    buffer_line_height = {
+        custom = 2;
+    };
+
+    vim_mode = false;
+
+    languages = {
+        TypeScript = {
+            show_inline_completions = true;
+            formatter = "prettier";
+            code_actions_on_format = {
+                "source.fixAll.eslint" = true;
+            };
+            format_on_save = {
+                external = {
+                command = "prettier";
+                arguments = ["--stdin-filepath" "{buffer_path}"];
+                };
+            };
+        };
+        TSX = {
+            formatter = "prettier";
+            code_actions_on_format = {
+                "source.fixAll.eslint" = true;
+            };
+            format_on_save = "on";
+        };
+        JavaScript = {
+            formatter = "prettier";
+            code_actions_on_format = {
+                "source.fixAll.eslint" = true;
+            };
+            format_on_save = "on";
+        };
+        Astro = {
+            formatter = "prettier";
+            code_actions_on_format = {
+                "source.fixAll.eslint" = true;
+            };
+            format_on_save = "on";
+        };
+    };
+
+    formatter = {
+        code_actions = {
+        "source.fixAll.eslint" = true;
+        };
+    };
+  };
+
+  keymap = [
+    {
+      context = "Editor";
+      bindings = {
+        "ctrl-'" = "editor::ToggleComments";
+      };
+    }
+  ];
+
+  extensions = [
+    "astro"
+    "everforest"
+    "html"
+    "nix"
+    "unocss"
+  ];
+in {
+  options.${namespace}.apps.zed = {enable = mkEnableOption "Enable Zed";};
+  config = mkIf cfg.enable {
+    programs.zed-editor = {
+        enable = true;
+        userSettings = settings;
+        userKeymaps = keymap;
+        extensions = extensions;
+    };
+  };
+}
