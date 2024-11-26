ba6681a51f6ba367178beeae9d91854b7436ef84
hyena <hyena@hyenabyte.dev>
2024-11-26 23:41:27 +0100

    chore(hyprland): WIP

--- landscape/modules/nixos/desktop/hyprland/default.nix
+++ landscape/modules/nixos/desktop/hyprland/default.nix
@@ -1,18 +1,28 @@
-{
-  pkgs,
-  lib,
-  config,
-  namespace,
-  ...
+{ lib
+, pkgs
+, config
+, namespace
+, ...
 }:
 with lib;
 with lib.${namespace}; let
   cfg = config.${namespace}.desktop.hyprland;
-in {
-  options.${namespace}.desktop.hyprland = {enable = mkEnableOption "hyprland";};
+in
+{
+  options.${namespace}.desktop.hyprland = { enable = mkEnableOption "hyprland"; };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-    ];
+    hyenabyte.system.xkb.enable = true;
+    hyenabyte.desktop.addons = {
+      wallpapers = enabled;
+      gtk = enabled;
+      rofi = {
+        enable = true;
+        package = pkgs.rofi-wayland;
+      };
+      waybar = enabled;
+    };
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     programs.hyprland = {
       enable = true;
