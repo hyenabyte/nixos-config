0685bb76be79ea9d62da76c3bc171f36d0143428
hyena <hyena@hyenabyte.dev>
2024-11-26 23:43:30 +0100

    feat(zsh): update zsh configs
    + addons
    + better config
    
    - antidote

--- landscape/modules/home/shell/zsh/default.nix
+++ landscape/modules/home/shell/zsh/default.nix
@@ -1,192 +1,102 @@
-{
-  pkgs,
-  lib,
-  config,
-  namespace,
-  ...
+{ pkgs
+, lib
+, config
+, namespace
+, ...
 }:
 with lib;
 with lib.${namespace}; let
   cfg = config.${namespace}.shell.zsh;
-in {
-  options.${namespace}.shell.zsh = {enable = mkEnableOption "shell.zsh";};
+  enabledWithZsh = enabled // {
+    enableZshIntegration = true;
+  };
+
+  plugins = [
+    {
+      name = "fzf-tab";
+      src = pkgs.zsh-fzf-tab.src;
+    }
+
+  ];
+in
+{
+  options.${namespace}.shell.zsh = { enable = mkEnableOption "Enable the ZSH shell"; };
   config = mkIf cfg.enable {
-    #packages = with pkgs; [
-    #  croc
-    #  ncdu
-    #  xh
-    #  xsel
-    #];
+    hyenabyte.shell.addons = {
+      bat = enabledWithZsh;
+      fzf = enabledWithZsh;
+      lsd = enabled // {
+        enableAliases = true;
+      };
+      starship = enabled;
+      zoxide = enabledWithZsh // {
+        enableCdAlias = true;
+      };
+    };
 
-    programs = {
-      zsh = {
-        enable = true;
+    programs.zsh = {
+      enable = true;
 
-        # directory to put config files in
-        dotDir = ".config/zsh";
+      inherit plugins;
 
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
+      # Automatically enter into a directory if typed directly into shell
+      autocd = true;
 
-        # Antidote
-        antidote = {
-          enable = true;
-          plugins = [
-            "zsh-users/zsh-completions"
-          ];
-        };
-
-        # .zshrc
-        initExtra = ''
-          if [ $(uname) = "Darwin" ]; then
-            path=("$HOME/.nix-profile/bin" "/run/wrappers/bin" "/etc/profiles/per-user/$USER/bin" "/nix/var/nix/profiles/default/bin" "/run/current-system/sw/bin" "/opt/homebrew/bin" $path)
-          fi
-        '';
-
-        # Tweak settings for history
-        history = {
-          save = 1000;
-          size = 1000;
-          path = "$HOME/.cache/zsh_history";
-        };
-
-        # Set some aliases
-        shellAliases = {
-          ".." = "cd ..";
-          c = "xsel -i";
-          v = "xsel -o";
-          cat = "bat";
-          diff = "batdiff";
-          man = "batman";
-          grep = "batgrep";
-          watch = "batwatch";
-        };
+      # An attribute set that adds to named directory hash table
+      dirHashes = {
+        nix = "$HOME/workspace/nixos-config";
+        work = "$HOME/workspace";
+        dl = "$HOME/Downloads";
+        docs = "$HOME/Documents";
       };
 
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
+      # directory to put config files in
+      dotDir = ".config/zsh";
+
+      # Enable zsh completion
+      enableCompletion = true;
+
+      # Options related to commands history configuration
+      history = {
+        save = 4096;
+        size = 4096;
+        path = "$HOME/.cache/zsh_history";
       };
 
-      # zoxide replaces cd
-      zoxide = {
-        enable = true;
-        enableZshIntegration = true;
+      # Extra commands that should be added to .zshrc
+      initExtra = ''
+        if [ $(uname) = "Darwin" ]; then
+          path=("$HOME/.nix-profile/bin" "/run/wrappers/bin" "/etc/profiles/per-user/$USER/bin" "/nix/var/nix/profiles/default/bin" "/run/current-system/sw/bin" "/opt/homebrew/bin" $path)
+          eval "''$(/opt/homebrew/bin/brew shellenv)"
+        fi
 
-        options = ["--cmd" "cd"];
+        # Keybinds
+        bindkey '^ ' autosuggest-accept
+
+        # Completion styles
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+        zstyle ':completion:*' menu no
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+      '';
+
+      # Set some aliases
+      shellAliases = {
+        ".." = "cd ..";
+        "lg" = "lazygit";
+        "zed" = "zeditor";
       };
 
-      # bat replaces cat
-      bat = {
+      autosuggestion = {
         enable = true;
-        extraPackages = with pkgs.bat-extras; [
-          batdiff
-          batman
-          batgrep
-          batwatch
+        strategy = [
+          "history"
+          "completion"
         ];
       };
 
-      # lsd replaceses ls
-      lsd = {
-        enable = true;
-        enableAliases = true;
-      };
-
-      # Starship for the prompt
-      starship = {
-        enable = true;
-
-        settings = {
-          right_format = "$git_branch$git_state$git_status$shell$nix_shell";
-
-          format = "$username$hostname$directory$character$cmd_duration[](red)[](yellow)[](green) ";
-
-          # $line_break\
-
-          add_newline = false;
-
-          username = {
-            style_user = "purple bold";
-            style_root = "red bold";
-            format = "[$user]($style) ";
-            disabled = false;
-            show_always = true;
-          };
-
-          hostname = {
-            ssh_only = true;
-            format = "[$ssh_symbol](bold blue)@[$hostname](bold red) ";
-            trim_at = ".local";
-            disabled = false;
-          };
-
-          directory = {
-            style = "blue";
-            read_only = " ";
-            truncation_length = 2;
-            fish_style_pwd_dir_length = 1;
-            # truncate_to_repo = false;
-            # truncation_symbol = "/";
-          };
-
-          character = {
-            format = "$symbol";
-            success_symbol = "";
-            error_symbol = "[󱎘 ](red)";
-            vimcmd_symbol = "[ ](green)";
-          };
-
-          shell = {
-            # disabled = false;
-            format = "[$indicator]($style) ";
-            style = "cyan";
-            zsh_indicator = "zsh";
-            fish_indicator = "󰈺";
-            powershell_indicator = ">_";
-          };
-
-          git_branch = {
-            format = "[$branch ]($style)";
-            style = "bright-black";
-          };
-
-          git_status = {
-            # format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-            format = "([\\[$all_status$ahead_behind\\]]($style) )";
-            style = "cyan";
-            # conflicted = "​";
-            # untracked = "​";
-            modified = "";
-            # staged = "​";
-            # renamed = "​";
-            deleted = "󱎘";
-            stashed = "≡";
-          };
-
-          git_state = {
-            format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
-            style = "bright-black";
-          };
-
-          cmd_duration = {
-            format = "[$duration]($style) ";
-            style = "yellow";
-          };
-
-          nix_shell = {
-            format = "[$symbol $state]($style)";
-            symbol = "";
-          };
-        };
-      };
-
-      # Yazi file manager
-      yazi = {
-        enable = true;
-        enableZshIntegration = true;
-      };
+      syntaxHighlighting = enabled;
     };
   };
 }
