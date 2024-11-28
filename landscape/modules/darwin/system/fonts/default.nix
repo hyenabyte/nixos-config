02984d28b1d91a44540f5435860772bdb3285519
hyena <hyena@hyenabyte.dev>
2024-11-28 14:46:21 +0100

    feat(darwin-system): adding fotns

---
+++ landscape/modules/darwin/system/fonts/default.nix
@@ -0,0 +1,31 @@
+{ options
+, pkgs
+, lib
+, config
+, namespace
+, ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.system.fonts;
+in
+{
+  options.${namespace}.system.fonts = with types; {
+    enable = mkEnableOption "fonts";
+    fonts = mkOpt (listOf package) [ ] "Extra fonts to install.";
+  };
+  config = mkIf cfg.enable {
+    environment.variables = {
+      # Enable icons in tooling since we have nerdfonts.
+      LOG_ICONS = "true";
+    };
+    fonts = {
+      packages = with pkgs;
+        [
+          atkinson-hyperlegible
+          (nerdfonts.override { fonts = [ "Agave" "Iosevka" "FiraCode" ]; })
+        ]
+        ++ cfg.fonts;
+    };
+  };
+}
