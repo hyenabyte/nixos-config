016caed0f842c88747b260540574db474e00dca2
hyena <hyena@hyenabyte.dev>
2024-11-28 14:45:18 +0100

    feat(darwin-app): adding karabiner-elements

---
+++ landscape/modules/darwin/apps/karabiner-elements/default.nix
@@ -0,0 +1,18 @@
+{ pkgs
+, lib
+, config
+, namespace
+, ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.apps.karabiner-elements;
+in
+{
+  options.${namespace}.apps.karabiner-elements = { enable = mkEnableOption "Karabiner Elements"; };
+  config = mkIf cfg.enable {
+    environment.packages = with pkgs; [
+      karabiner-elements
+    ];
+  };
+}
