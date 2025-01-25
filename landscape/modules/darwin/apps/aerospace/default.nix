e1c2be5bd6b7d870f265be56dae2143993c25705
hyena <hyena@hyenabyte.dev>
2025-01-25 20:28:33 +0100

    chore(aerospace): update config

--- landscape/modules/darwin/apps/aerospace/default.nix
+++ landscape/modules/darwin/apps/aerospace/default.nix
@@ -11,8 +11,81 @@ in
 {
   options.${namespace}.apps.aerospace = { enable = mkEnableOption "Aerospace"; };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      aerospace
-    ];
+    services.aerospace = {
+      enable = true;
+      settings = {
+        start-at-login = false;
+
+        after-login-command = [ ];
+        after-startup-command = [ ];
+
+        gaps = {
+          inner.vertical = 5;
+          inner.horizontal = 5;
+          outer.left = 5;
+          outer.right = 5;
+          outer.top = 5;
+          outer.bottom = 5;
+        };
+
+        mode.main.binding = {
+          alt-slash = "layout tiles horizontal vertical";
+          alt-comma = "layout accordion horizontal vertical";
+
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
+
+          alt-shift-h = "move left";
+          alt-shift-j = "move down";
+          alt-shift-k = "move up";
+          alt-shift-l = "move right";
+
+          alt-shift-minus = "resize smart -50";
+          alt-shift-equal = "resize smart +50";
+
+          alt-1 = "workspace 1";
+          alt-2 = "workspace 2";
+          alt-3 = "workspace 3";
+          alt-4 = "workspace 4";
+          alt-5 = "workspace 5";
+          alt-6 = "workspace 6";
+          alt-7 = "workspace 7";
+          alt-8 = "workspace 8";
+          alt-9 = "workspace 9";
+
+          alt-shift-1 = "move-node-to-workspace 1";
+          alt-shift-2 = "move-node-to-workspace 2";
+          alt-shift-3 = "move-node-to-workspace 3";
+          alt-shift-4 = "move-node-to-workspace 4";
+          alt-shift-5 = "move-node-to-workspace 5";
+          alt-shift-6 = "move-node-to-workspace 6";
+          alt-shift-7 = "move-node-to-workspace 7";
+          alt-shift-8 = "move-node-to-workspace 8";
+          alt-shift-9 = "move-node-to-workspace 9";
+
+          alt-tab = "workspace-back-and-forth";
+          alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+          alt-shift-semicolon = "mode service";
+        };
+
+        mode.service.binding = {
+          esc = [ "reload-config" "mode main" ];
+          r = [ "flatten-workspace-tree" "mode main" ];
+          f = [ "layout floating tiling" "mode main" ];
+          backspace = [ "close-all-windows-but-current" "mode main" ];
+
+          alt-shift-h = [ "join-with left" "mode main" ];
+          alt-shift-j = [ "join-with down" "mode main" ];
+          alt-shift-k = [ "join-with up" "mode main" ];
+          alt-shift-l = [ "join-with right" "mode main" ];
+
+          # down = "volume down";
+          # up = "volume up";
+          # shift-down = [ "volume set 0" "mode main" ];
+        };
+      };
+    };
   };
 }
