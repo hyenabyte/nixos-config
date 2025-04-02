97a4c25096752ae0d057493ee4266d43967417d6
hyena <hyena@hyenabyte.dev>
2025-04-02 13:47:26 +0200

    chore(hypridle): update settings

--- landscape/modules/nixos/desktop/addons/hypridle/default.nix
+++ landscape/modules/nixos/desktop/addons/hypridle/default.nix
@@ -17,7 +17,29 @@ in
   config = mkIf cfg.enable {
     hyenabyte.home.services.hypridle = {
       enable = true;
-      settings = { };
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
     };
   };
 }
