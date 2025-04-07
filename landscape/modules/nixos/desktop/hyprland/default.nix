c6be12bebf3dbbf805f179e131e6058ea5d6a88e
hyena <hyena@hyenabyte.dev>
2025-04-07 16:04:18 +0200

    chore(hyprland): update config

--- landscape/modules/nixos/desktop/hyprland/default.nix
+++ landscape/modules/nixos/desktop/hyprland/default.nix
@@ -22,10 +22,24 @@ in
       waybar = enabled;
       hypridle = enabled;
       hyprlock = enabled;
-      dunst = enabled;
+      hyprpaper = enabled;
     };
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    environment.defaultPackages = with pkgs; [
+      nautilus
+      hyprpolkitagent
+      hyprpicker
+      hyprsunset
+      wl-clipboard
+      clipse
+      swaynotificationcenter
+      grim
+      slurp
+      loupe
+      pavucontrol
+      feh
+    ];
 
     programs.hyprland.enable = true;
 
@@ -34,14 +48,28 @@ in
         wayland.windowManager.hyprland = {
           enable = true;
 
+          # plugins = with pkgs.hyprlandPlugins; [
+          #   hy3
+          # ];
+
           settings =
             let
               terminal = "ghostty";
-              fileManager = "nautilus";
-              menu = "rofi -show drun";
+              zellij = "ghostty --class=com.${namespace}.zellij -e zellij -l welcome";
+              fileManager = "ghostty --class=com.${namespace}.yazi -e yazi";
+              fileManager-gui = "nautilus";
+              browser = "zen";
+              clipse = "ghostty --class=com.${namespace}.clipse -e clipse";
+              launcher = "rofi -show drun";
               lock = "hyprlock";
+              colorPicker = "hyprpicker -a";
+              screenshot = "grim -g \"$(slurp)\"";
+
+              signal = "signal-desktop --password-store=kwallet6";
 
               mainMod = "SUPER";
+
+              workspaceCount = 9;
             in
             {
               # Monitor setup
@@ -57,9 +85,14 @@ in
               ];
 
               exec-once = [
-                "waybar"
-                "hypridle"
-                "dunst"
+                "systemctl --user start hyprpolkitagent"
+                "killall -q waybar;sleep .5 && waybar"
+                "killall -q swaync;sleep .5 && swaync"
+                # "hypridle"
+                "hyprpaper"
+                # "hyprsunset"
+                # "dunst"
+                # "${signal}" # Kwallet doesnt unlock with greetd for some reason :c
               ];
 
               general = {
@@ -68,13 +101,17 @@ in
                 gaps_in = 5;
                 gaps_out = 10;
 
-                border_size = 1;
+                border_size = 2;
 
-                "col.active_border" = "0xff77afca";
-                "col.inactive_border" = "0xaa595959";
+                "col.active_border" = "rgb(ebe0bc)";
+                "col.inactive_border" = "rgba(595959aa)";
 
                 resize_on_border = false;
                 allow_tearing = false;
+              };
+
+              debug = {
+                disable_logs = false;
               };
 
               decoration = {
@@ -83,19 +120,8 @@ in
                 active_opacity = 1.0;
                 inactive_opacity = 1.0;
 
-                shadow = {
-                  enabled = true;
-                  range = 4;
-                  render_power = 3;
-                  color = "0xee1a1a1a";
-                };
-
-                blur = {
-                  enabled = false;
-                  size = 3;
-                  passes = 1;
-                  vibrancy = 0.1696;
-                };
+                shadow.enabled = true;
+                blur.enabled = false;
               };
 
               animations = {
@@ -116,7 +142,7 @@ in
                   "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
                   "windowsOut, 1, 1.49, linear, popin 87%"
                   "fadeIn, 1, 1.73, almostLinear"
-                  "animation = fadeOut, 1, 1.46, almostLinear"
+                  "fadeOut, 1, 1.46, almostLinear"
                   "fade, 1, 3.03, quick"
                   "layers, 1, 3.81, easeOutQuint"
                   "layersIn, 1, 4, easeOutQuint, fade"
@@ -139,15 +165,27 @@ in
               };
 
               misc = {
-                force_default_wallpaper = -1;
-                disable_hyprland_logo = false;
+                # disable_autoreload = true;
+                force_default_wallpaper = 2;
+                disable_hyprland_logo = true;
+                font_family = "Atkinson Hyperlegible";
+                animate_manual_resizes = true;
+                background_color = "rgb(09090a)";
+
+                # DPMS
+                mouse_move_enables_dpms = true;
+                key_press_enables_dpms = true;
+
+                # Splash
+                disable_splash_rendering = true;
+                splash_font_family = "Atkinson Hyperlegible";
               };
 
               input = {
                 kb_layout = "dk";
                 kb_variant = "";
                 kb_model = "";
-                kb_options = "";
+                kb_options = "caps:escape";
                 kb_rules = "";
 
                 follow_mouse = 1;
@@ -161,16 +199,35 @@ in
 
               bind = [
                 # General bindings
-                "${mainMod}, T, exec, ${terminal}"
-                "${mainMod}, Return, exec, ${terminal}"
+                "${mainMod} SHIFT, M, exit"
+                "${mainMod}, X, exec, ${lock}"
+
                 "${mainMod}, Q, killactive,"
-                "${mainMod}, Q, SHIFT, exec, ${lock}"
-                "${mainMod}, M, exit,"
-                "${mainMod}, F, exec, ${fileManager}"
-                "${mainMod}, V, togglefloating,"
-                "${mainMod}, D, exec, ${menu}"
+                # "${mainMod} SHIFT, Q, forcekillactive," # FIXME: dispather missing in current version
+
+                # Apps
+                "${mainMod}, Space, exec, ${launcher}"
+                "${mainMod}, D, exec, ${launcher}"
+                "${mainMod}, E, exec, ${fileManager}"
+                "${mainMod} SHIFT, E, exec, ${fileManager-gui}"
+                "${mainMod}, I, exec, ${colorPicker},"
+                "${mainMod}, T, exec, ${terminal}"
+                "${mainMod}, Return, exec, ${zellij}"
+                "${mainMod} SHIFT, Return, exec, ${terminal}"
+                "${mainMod}, V, exec, ${clipse}"
+                "${mainMod}, W, exec, ${browser}"
+                "${mainMod}, I, exec, ${screenshot}"
+                "${mainMod}, Print, exec, ${screenshot}"
+                "${mainMod}, O, exec, pavucontrol"
+
+
+                # Misc window movement
+                "${mainMod}, F, fullscreen,"
+                "${mainMod} SHIFT, F, togglefloating,"
+                "${mainMod}, C, centerwindow"
                 "${mainMod}, P, pseudo,"
                 "${mainMod}, B, toggleSplit,"
+                "${mainMod} SHIFT, W, swapactiveworkspaces, DP-2 HDMI-A-2"
 
                 # Move focus
                 "${mainMod}, left, movefocus, l"
@@ -195,11 +252,14 @@ in
                 # Example special workspace (scratchpad)
                 "${mainMod}, S, togglespecialworkspace, magic"
                 "${mainMod} SHIFT, S, movetoworkspace, special:magic"
+                "${mainMod}, A, togglespecialworkspace, term"
+                "${mainMod} SHIFT, A, movetoworkspace, special:term"
 
 
                 # Scroll through workspaces
-                "${mainMod}, mouse_down, workspace, e+1"
-                "${mainMod}, mouse_up, workspace, e-1"
+                # "${mainMod}, mouse_down, workspace, e+1"
+                # "${mainMod}, mouse_up, workspace, e-1"
+
               ] ++ (
                 # workspaces
                 # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
@@ -209,9 +269,10 @@ in
                     in [
                       "${mainMod}, code:1${toString i}, workspace, ${toString ws}"
                       "${mainMod} SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                      "${mainMod} CTRL, code:1${toString i}, movetoworkspacesilent, ${toString ws}"
                     ]
                   )
-                  9)
+                  workspaceCount)
               );
 
               bindm = [
@@ -219,14 +280,103 @@ in
                 "${mainMod}, mouse:273, resizeWindow"
               ];
 
+              bindl = [
+                # Laptop multimedia keys for volume and LCD brightness
+                " ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+                " ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+                " ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+                " ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+                " ,XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+                " ,XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+                # Requires playerctl
+                " , XF86AudioNext, exec, playerctl next"
+                " , XF86AudioPause, exec, playerctl play-pause"
+                " , XF86AudioPlay, exec, playerctl play-pause"
+                " , XF86AudioPrev, exec, playerctl previous"
+              ];
+
               windowrulev2 = [
                 "suppressevent maximize, class:.*"
                 "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+
+                # Float pavucontrol
+                "float, class:^(org.pulseaudio.pavucontrol)$"
+                "size 750 500, class:^(org.pulseaudio.pavucontrol)$"
+
+                # Float clipse
+                "float, class:(com.${namespace}.clipse)"
+                "size 750 500, class:(com.${namespace}.clipse)"
+                "stayfocused, class:(com.${namespace}.clipse)"
+
+                # Float Picture-in-Picture windows
+                "float, title:^(Picture-in-Picture)$"
+                "size 750 500, class:(Picture-in-Picture)"
+
+                # Steam
+                "float, title:^(Friends List)$"
+                "size 750 500, class:(Friends List)"
+              ];
+
+              workspace = [
+                "1, monitor:desc:LG Electronics LG ULTRAGEAR 209MAQQFWE16, default:true"
+                "2, monitor:desc:AOC Q27P1B GNXJCHA039883, default:true"
               ];
             };
+
+          extraConfig =
+            let
+              mainMod = "SUPER";
+            in
+            ''
+              # Resize mode
+              bind = ${mainMod}, N, submap, resize
+
+              submap = resize
+
+              binde = , right, resizeactive, 100 0
+              binde = , left, resizeactive, -100 0
+              binde = , up, resizeactive, 0 -100
+              binde = , down, resizeactive, 0 100
+              binde = , L, resizeactive, 100 0
+              binde = , H, resizeactive, -100 0
+              binde = , K, resizeactive, 0 -100
+              binde = , J, resizeactive, 0 100
+
+              # precision mode
+              binde = SHIFT, right, resizeactive, 10 0
+              binde = SHIFT, left, resizeactive, -10 0
+              binde = SHIFT, up, resizeactive, 0 -10
+              binde = SHIFT, down, resizeactive, 0 10
+              binde = SHIFT, L, resizeactive, 10 0
+              binde = SHIFT, H, resizeactive, -10 0
+              binde = SHIFT, K, resizeactive, 0 -10
+              binde = SHIFT, J, resizeactive, 0 10
+
+              bind = , escape, submap, reset
+              bind = , return, submap, reset
+              bind = ${mainMod}, N, submap, reset
+              submap = reset
+
+              bind = ${mainMod}, Z, submap, power
+              submap = power
+              
+              bind = , L, exit
+              bind = , X, exec, hyprlock
+              bind = , S, exec, systemctl suspend
+              bind = , R, exec, systemctl reboot
+              bind = , Q, exec, systemctl poweroff
+
+              # Reset submap on suspend and lock, so it doesnt restart into power submap mode
+              bind = , X, submap, reset
+              bind = , S, submap, reset
+
+              bind = , escape, submap, reset
+              bind = , return, submap, reset
+              bind = ${mainMod}, Z, submap, reset
+              submap = reset
+            '';
         };
       };
     };
-
   };
 }
