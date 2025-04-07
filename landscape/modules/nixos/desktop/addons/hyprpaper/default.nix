f1b5ec2336909ee710f2b42882e08ffea2ec5f79
hyena <hyena@hyenabyte.dev>
2025-04-07 16:39:21 +0200

    chore(hyprpaper): now uses wallpapers from wallpaper package

--- landscape/modules/nixos/desktop/addons/hyprpaper/default.nix
+++ landscape/modules/nixos/desktop/addons/hyprpaper/default.nix
@@ -8,16 +8,33 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.desktop.addons.hyprpaper;
+  # inherit (pkgs."${namespace}") wallpapers;
+
+  wallpapers = pkgs."${namespace}".wallpapers;
+
 in
 {
   options.${namespace}.desktop.addons.hyprpaper = with types; {
     enable = mkEnableOption "Hyprpaper";
+    wallpaper = mkOpt str "nix-logo-everforest.png" "The wallpaper from the wallpaper package to use";
   };
 
   config = mkIf cfg.enable {
     hyenabyte.home.services.hyprpaper = {
       enable = true;
-      settings = { };
+
+      settings =
+        let
+          wallpaper = "${wallpapers}/share/wallpapers/${wallpaper}";
+        in
+        {
+          ipc = true;
+          splash = false;
+          preload = [ "${wallpaper}" ];
+          wallpaper = [
+            ",${wallpaper}"
+          ];
+        };
     };
   };
 }
