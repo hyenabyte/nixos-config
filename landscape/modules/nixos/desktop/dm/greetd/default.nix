7333aa3e0e84d5f88aae7e1bbdcb5f075a041805
hyena <hyena@hyenabyte.dev>
2025-04-07 15:11:54 +0200

    chore(greetd): update config

--- landscape/modules/nixos/desktop/dm/greetd/default.nix
+++ landscape/modules/nixos/desktop/dm/greetd/default.nix
@@ -8,21 +8,55 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.desktop.dm.greetd;
+
 in
 {
   options.${namespace}.desktop.dm.greetd = with types; {
     enable = mkEnableOption "Greetd";
-    command = mkOpt types.str "Hyprland" "The Command for greetd to default to";
+    command = mkOpt str "Hyprland" "The Command for greetd to default to";
+    enableKwallet = mkEnableOption "Unlock kwallet on login";
   };
 
   config = mkIf cfg.enable {
+    security.pam = {
+      services.greetd = {
+        enableKwallet = mkIf cfg.enableKwallet true;
+      };
+    };
+
     services.greetd = {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${cfg.command}";
+          command =
+            let
+              mapTheme = attr: foldlAttrs (acc: name: value: acc + "${name}=${value};") "" attr;
+              theme = {
+                text = "cyan";
+                # time = "cyan";
+                container = "darkgray";
+                border = "white";
+                # title = "cyan";
+                greet = "green";
+                prompt = "gray";
+                input = "white";
+                # action = "cyan";
+                button = "yellow";
+              };
+            in
+            ''
+              ${pkgs.greetd.tuigreet}/bin/tuigreet \
+                --theme '${mapTheme theme}' \
+                --width 60 --window-padding 1 --container-padding 2 --prompt-padding 0 \
+                --time --remember \
+                --greeting 'welcome' \
+                --cmd ${cfg.command}
+            '';
+
+          user = "greeter";
         };
       };
     };
+
   };
 }
