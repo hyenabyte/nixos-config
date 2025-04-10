18e94dc77ada65fe23e6c749ded36a8baf1725b2
hyena <hyena@hyenabyte.dev>
2025-04-10 23:39:09 +0200

    chore(steam): add gamemode mangohud and gamescope

--- landscape/modules/nixos/apps/steam/default.nix
+++ landscape/modules/nixos/apps/steam/default.nix
@@ -9,14 +9,24 @@ with lib.${namespace}; let
   cfg = config.${namespace}.apps.steam;
 in
 {
-  options.${namespace}.apps.steam = { enable = mkEnableOption "steam"; };
+  options.${namespace}.apps.steam = {
+    enable = mkEnableOption "steam";
+    enableGamemode = mkOpt types.bool true "Enable gamemode";
+  };
   config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.mangohud pkgs.protonup ];
     programs.steam = {
       enable = true;
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
 
       # localNetworkGameTransfers.openFirewall = true;
       # remotePlay.openFirewall = true;
+
+      gamescopeSession.enable = true;
+    };
+
+    programs.gamemode = mkIf cfg.enableGamemode {
+      enable = true;
     };
   };
 }
