60639c60e629ad94efdfec4c083b35b88430cf02
hyena <hyena@hyenabyte.dev>
2025-04-10 11:08:50 +0200

    feat(security/keyring): adding gnome-keyring module

---
+++ landscape/modules/nixos/security/keyring/gnome-keyring/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, pkgs
+, config
+, namespace
+, ...
+}:
+with lib;
+with lib.${namespace}; let
+  cfg = config.${namespace}.security.keyring.gnome-keyring;
+in
+{
+  options.${namespace}.security.keyring.gnome-keyring = {
+    enable = mkEnableOption "gnome-keyring";
+    enableSeahorse = mkEnableOption "Seahorse";
+    enableGreetd = mkOpt types.bool false "Unlock keyring on login with greetd";
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = mkIf cfg.enableSeahorse [ pkgs.seahorse ];
+    services.gnome.gnome-keyring = {
+      enable = true;
+    };
+
+    services.greetd = mkIf cfg.enableGreetd {
+      enableGnomeKeyring = true;
+    };
+  };
+}
