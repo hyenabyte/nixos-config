03435e29fbe70b2429e92ac1f2b0b4346e46a10a
hyena <hyena@hyenabyte.dev>
2025-05-06 17:06:04 +0200

    chore(wireless): adding some iwd related options

--- landscape/modules/nixos/hardware/wireless/default.nix
+++ landscape/modules/nixos/hardware/wireless/default.nix
@@ -11,12 +11,32 @@ in
 {
   options.${namespace}.hardware.wireless = with types; {
     enable = mkEnableOption "Enable wireless";
+    enableIwd = mkOpt bool false "Enable iwd";
   };
 
   config = mkIf cfg.enable {
-    networking.wireless = {
-      enable = true; # Enables wireless support via wpa_supplicant.
-      userControlled.enable = true; # Fixes error on switch when trying to restart wpa_supplicant
+    networking = {
+      wireless = {
+        enable = mkUnless cfg.enableIwd true; # Enables wireless support via wpa_supplicant.
+        userControlled.enable = true; # Fixes error on switch when trying to restart wpa_supplicant
+
+        iwd = mkIf cfg.enableIwd {
+          enable = true;
+          settings = {
+            Network = {
+              EnableIPv6 = true;
+              RoutePriorityOffset = 300;
+            };
+            Settings = {
+              AutoConnect = true;
+              Hidden = true;
+              AlwaysRandomizeAddress = false;
+            };
+          };
+        };
+      };
+      networkmanager.wifi.backend = mkIf cfg.enableIwd "iwd";
+      interfaces = { };
     };
   };
 }
