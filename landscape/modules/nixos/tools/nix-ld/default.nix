52a024e0c7cb7d5d45438f481d15892f50ddd94b
hyena <hyena@hyenabyte.dev>
2025-05-06 16:50:23 +0200

    chore(nix-ld): adding common libraries

--- landscape/modules/nixos/tools/nix-ld/default.nix
+++ landscape/modules/nixos/tools/nix-ld/default.nix
@@ -1,4 +1,5 @@
 { lib
+, pkgs
 , config
 , namespace
 , ...
@@ -10,6 +11,67 @@ in
 {
   options.${namespace}.tools.nix-ld = { enable = mkEnableOption "nix-ld"; };
   config = mkIf cfg.enable {
-    programs.nix-ld.enable = true;
+    programs.nix-ld = {
+      enable = true;
+
+      libraries = with pkgs; [
+        acl
+        attr
+        bzip2
+        dbus
+        expat
+        fontconfig
+        freetype
+        fuse3
+        icu
+        libnotify
+        libsodium
+        libssh
+        libunwind
+        libusb1
+        libuuid
+        nspr
+        nss
+        stdenv.cc.cc
+        util-linux
+        zlib
+        zstd
+      ] ++ lib.optionals (config.hardware.graphics.enable) [
+        pipewire
+        cups
+        libxkbcommon
+        pango
+        mesa
+        libdrm
+        libglvnd
+        libpulseaudio
+        atk
+        cairo
+        alsa-lib
+        at-spi2-atk
+        at-spi2-core
+        gdk-pixbuf
+        glib
+        gtk3
+        libGL
+        libappindicator-gtk3
+        vulkan-loader
+        xorg.libX11
+        xorg.libXScrnSaver
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.libXtst
+        xorg.libxcb
+        xorg.libxkbfile
+        xorg.libxshmfence
+        xorg.libXinerama
+      ];
+    };
   };
 }
