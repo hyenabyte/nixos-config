dcb34f849933981dcc8c7d6f55e518c3606fee66
hyena <hyena@hyenabyte.dev>
2025-06-07 19:08:34 +0200

    chore(hyprlock): update look and feel

--- landscape/modules/nixos/desktop/addons/hyprlock/default.nix
+++ landscape/modules/nixos/desktop/addons/hyprlock/default.nix
@@ -18,50 +18,83 @@ in
     security.pam.services.hyprlock = { };
     hyenabyte.home.programs.hyprlock = {
       enable = true;
-      settings = {
-        general = {
-          no_fade_in = false;
-          no_fade_out = false;
-          hide_cursor = true;
-          grace = 0;
-          disable_loading_bar = true;
-          ignore_empty_input = true;
+      settings =
+        let
+          font = "Maple Mono NF";
+        in
+        {
+          general = {
+            hide_cursor = true;
+            grace = 5;
+            disable_loading_bar = true;
+            ignore_empty_input = true;
+          };
+
+          animations = {
+            enable = true;
+            bezier = "linear, 1, 1, 0, 0";
+            animation = [
+              "fadeIn, 1, 5, linear"
+              "fadeOut, 1, 5, linear"
+              "inputFieldDots, 1, 2, linear"
+            ];
+          };
+
+          background = {
+            blur_passes = 3;
+            blur_size = 10;
+            contrast = 1;
+            brightness = 0.5;
+            vibrancy = 0.2;
+            vibrancy_darkness = 0.2;
+          };
+
+          label = [
+            {
+              text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 22;
+              font_family = font;
+              position = "0, 300";
+              halign = "center";
+              valign = "center";
+            }
+            {
+              text = "cmd[update:1000] echo \"$(date +\"%-I:%M\")\"";
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 95;
+              font_family = font;
+              position = "0, 200";
+              halign = "center";
+              valign = "center";
+            }
+          ];
+
+          input-field = {
+            size = "250, 60";
+            outline_thickness = 2;
+
+            dots_size = 0.2;
+            dots_spacing = 0.35;
+            dots_center = true;
+
+            font_family = font;
+
+            fade_on_empty = false;
+            rounding = -1;
+            placeholder_text = ''
+              <span foreground="##ebe0bc">Locked</span>
+            '';
+            hide_input = false;
+            position = "0, -200";
+            halign = "center";
+            valign = "center";
+
+            fail_text = "<b>$ATTEMPTS</b>";
+            fail_timeout = 2000;
+            fail_transition = 300;
+          };
         };
-
-        # background = {
-        #   color = "rgb(09090a)";
-        # };
-
-        input-field = {
-          size = "250, 60";
-          outline_thickness = 2;
-
-          dots_size = 0.2;
-          dots_spacing = 0.35;
-          dots_center = true;
-
-          # outer_color = "rgba(ebe0bcff)";
-          # inner_color = "rgba(09090aff)";
-
-          # font_color = "rgb(ebe0bc)";
-          font_family = "Atkinson Hyperlegible";
-
-          fade_on_empty = false;
-          rounding = -1;
-          placeholder_text = ''
-            <span foreground="##ebe0bc">Locked</span>
-          '';
-          hide_input = false;
-          position = "0, -200";
-          halign = "center";
-          valign = "center";
-          # check_color = "rgb(407467)";
-          # fail_color = "rgb(97484f)";
-          fail_text = "<b>$ATTEMPTS</b>";
-          fail_timeout = 2000;
-          fail_transition = 300;
-        };
-      };
     };
   };
 }
